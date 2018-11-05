60c7cab1781a9571d474351514572f7b8dbb2a80
ertrii <ertriilive@gmail.com>
2018-11-05 18:46:59 -0500

    fragments

--- app/src/main/java/me/ertrii/moonered/Activities/MainActivity.java
+++ app/src/main/java/me/ertrii/moonered/Activities/MainActivity.java
@@ -2,6 +2,8 @@ package me.ertrii.moonered.Activities;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +14,10 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.support.v7.widget.Toolbar;
 
+import me.ertrii.moonered.Fragments.MainFragment;
+import me.ertrii.moonered.Fragments.PayFragment;
+import me.ertrii.moonered.Fragments.RegisterFragment;
+import me.ertrii.moonered.Fragments.StatusFragment;
 import me.ertrii.moonered.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -38,6 +44,22 @@ public class MainActivity extends AppCompatActivity {
                 else menuItem.setCheckable(true);
 
                 drawerLayout.closeDrawers();
+
+                switch (menuItem.getItemId()){
+                    case R.id.main:
+                        setFragment(0);
+                        break;
+                    case R.id.status:
+                        setFragment(1);
+                        break;
+                    case R.id.pay:
+                        setFragment(2);
+                        break;
+                    case R.id.register:
+                        setFragment(3);
+                        break;
+                }
+
                 return false;
             }
         });
@@ -56,5 +78,32 @@ public class MainActivity extends AppCompatActivity {
 
         drawerLayout.setDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
+    }
+
+    public void setFragment(int position){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        switch (position){
+            case 0:
+                MainFragment mainFragment = new MainFragment();
+                fragmentTransaction.replace(R.id.fragment_layout, mainFragment);
+                fragmentTransaction.commit();
+                break;
+            case 1:
+                StatusFragment statusFragment = new StatusFragment();
+                fragmentTransaction.replace(R.id.fragment_layout, statusFragment);
+                fragmentTransaction.commit();
+                break;
+            case 2:
+                PayFragment payFragment = new PayFragment();
+                fragmentTransaction.replace(R.id.fragment_layout, payFragment);
+                fragmentTransaction.commit();
+                break;
+            case 3:
+                RegisterFragment registerFragment = new RegisterFragment();
+                fragmentTransaction.replace(R.id.fragment_layout, registerFragment);
+                fragmentTransaction.commit();
+                break;
+        }
     }
 }
---
+++ app/src/main/java/me/ertrii/moonered/Fragments/StatusFragment.java
@@ -0,0 +1,21 @@
+package me.ertrii.moonered.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import me.ertrii.moonered.R;
+
+public class StatusFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView;
+        rootView = inflater.inflate(R.layout.fragment_status, container, false);
+        return rootView;
+    }
+}
