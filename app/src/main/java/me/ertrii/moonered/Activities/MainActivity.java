e161852e499d2d11f6170a53669b758603c97af9
ertrii <ertriilive@gmail.com>
2018-11-05 15:50:01 -0500

    menu

--- app/src/main/java/me/ertrii/moonered/Activities/MainActivity.java
+++ app/src/main/java/me/ertrii/moonered/Activities/MainActivity.java
@@ -1,30 +1,60 @@
 package me.ertrii.moonered.Activities;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
 
 import me.ertrii.moonered.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnSession;
-    TextView txtNotice;
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnSession =  findViewById(R.id.btnSession);
-        txtNotice = findViewById(R.id.txtNotice);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        btnSession.setOnClickListener(new View.OnClickListener() {
+        drawerLayout = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.navigation);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                txtNotice.setText("This is a notice");
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if(menuItem.isChecked()) menuItem.setCheckable(false);
+                else menuItem.setCheckable(true);
+
+                drawerLayout.closeDrawers();
+                return false;
             }
         });
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
     }
 }
